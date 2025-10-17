789bbe844b6d5509efdf754d778978c2fe4cc91f
Smook-e <ahmednaser2382005@gmail.com>
2025-10-17 08:45:56 +0300

    added new classes and files

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.remove(2);
+        System.out.println(list);
 
     }
 }
