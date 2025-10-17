45125b6a11e391346dff7c6b05b6acdae355980d
EngYoussefSharaf <sharafyoussef693@gmail.com>
2025-10-18 02:37:31 +0300

    update

--- src/CustomerProduct.java
+++ src/CustomerProduct.java
@@ -1,12 +1,50 @@
-public class CustomerProduct implements Record{
+import java.time.LocalDate;
 
+public class CustomerProduct implements Record {
+    private String customerSSN;
+    private String productID;
+    private LocalDate purchaseDate;
+    private boolean paid;
 
-    @Override
-    public String lineRepresentation() {
-        return "";
+    public CustomerProduct(String customerSSN, String productID, LocalDate purchaseDate) {
+        this.customerSSN = customerSSN;
+        this.productID = productID;
+        this.purchaseDate = purchaseDate;
+        this.paid = false;
+    }
+
+    public String getCustomerSSN() {
+        return customerSSN;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public LocalDate getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public boolean isPaid() {
+        return paid;
+    }
+
+    public void setPaid(boolean paid) {
+        this.paid = paid;
+    }
+    public static String reverseDate(String Date)
+    {
+        String[] tokens = Date.split("-");
+        String revDate = tokens[2]+"-"+tokens[1]+"-"+tokens[0];
+        return revDate;
     }
     @Override
+    public String lineRepresentation() {
+        return customerSSN+","+productID+","+reverseDate(purchaseDate.toString())+","+paid;
+    }
+
+    @Override
     public String getSearchKey() {
-        return "";
+        return customerSSN+","+productID+","+reverseDate(purchaseDate.toString());
     }
 }
