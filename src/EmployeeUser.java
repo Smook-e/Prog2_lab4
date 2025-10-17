d9f8c3db5cb84fabb0eddb5d5d0176cde5259b5e
Smook-e <ahmednaser2382005@gmail.com>
2025-10-17 13:33:09 +0300

    added EmployeeUser and EmployeeUserDatabase

--- src/EmployeeUser.java
+++ src/EmployeeUser.java
@@ -1,10 +1,24 @@
 public class EmployeeUser implements Record{
+    private String employeeId;
+    private String name;
+    private String email;
+    private String address;
+    private String phoneNumber;
+
+    public EmployeeUser(String employeeId, String name, String email, String address, String phoneNumber) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
     @Override
     public String lineRepresentation() {
-        return "";
+        return employeeId + "," + name + "," + email + "," + address + "," + phoneNumber;
     }
     @Override
     public String getSearchKey() {
-        return "";
+        return employeeId;
     }
 }
--- src/EmployeeUserDatabase.java
+++ src/EmployeeUserDatabase.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class EmployeeUserDatabase extends DataBase {
 
     public EmployeeUserDatabase(String filename) {
@@ -6,11 +11,31 @@ public class EmployeeUserDatabase extends DataBase {
 
     @Override
     public void readFromFile() {
-
+        try(BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            records.clear();
+            while ((line = br.readLine()) != null) {
+                Record r = createRecordFrom(line);
+                if(r != null) {
+                    records.add(r);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Record createRecordFrom(String line) {
-        return null;
+        String[] data = line.split(",");
+        Record record;
+        record = new EmployeeUser(data[0], data[1], data[2], data[3], data[4]);
+
+
+        return record;
     }
 }
